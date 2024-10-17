0eb0cdf83a9c6191a36259b87dc12abab53c87af
Darren <darren_li_osa@hotmail.com>
2024-10-17 16:51:40 -0700

    update

--- update.py
+++ update.py
@@ -1,8 +1,7 @@
 ﻿import datetime
 import time
 from xml.dom import minidom
-from performance import ltm_performance, total_performance, beta, sharpe, treynor, max_drawdown
-
+import pandas as pd
 
 def daily_readme(date):
     """
@@ -42,19 +41,19 @@ def perf_counter(funct, *args):
     funct_return = funct(*args)
     return funct_return, time.perf_counter() - start
 
-def svg_overwrite(filename, age_data, ltm_data, total_data, beta_data, sharpe_data, treynor_data, drawdown_data):
+def svg_overwrite(filename, age_data, cagr_data, total_data, alpha_data, beta_data, sharpe_data, drawdown_data):
     """
     Parse SVG files and update elements with my age, commits, stars, repositories, and lines written
     """
     svg = minidom.parse(filename)
     f = open(filename, mode='w', encoding='utf-8')
     tspan = svg.getElementsByTagName('tspan')
-    tspan[50].firstChild.data = ltm_data
+    tspan[50].firstChild.data = cagr_data
     tspan[53].firstChild.data = age_data
     tspan[55].firstChild.data = total_data
-    tspan[57].firstChild.data = beta_data
-    tspan[59].firstChild.data = sharpe_data
-    tspan[61].firstChild.data = treynor_data
+    tspan[57].firstChild.data = alpha_data
+    tspan[59].firstChild.data = beta_data
+    tspan[61].firstChild.data = sharpe_data
     tspan[63].firstChild.data = drawdown_data
     f.write(svg.toxml('utf-8').decode('utf-8'))
     f.close()
@@ -63,5 +62,12 @@ if __name__ == '__main__':
     print('Calculation times:')
     age_data, age_time = perf_counter(daily_readme, datetime.datetime(2022, 7, 1))
     formatter('age calculation', age_time)
-    svg_overwrite('dark_ver.svg', age_data, ltm_performance, total_performance, beta, sharpe, treynor, max_drawdown)
-    svg_overwrite('light_ver.svg', age_data, ltm_performance, total_performance, beta, sharpe, treynor, max_drawdown)
+    data = pd.read_csv('performance.csv', header=None, index_col=0)[1].to_dict()
+    cagr = "{}%".format(round(((data['total_performance']/100+1)**(365/int(age_data))-1)*100,2))
+    total_performance = "{}%".format(data['total_performance'])
+    alpha = "{}".format(data['alpha'])
+    beta = "{}".format(data['beta'])
+    sharpe = "{}".format(data['sharpe'])
+    max_drawdown = "{}%".format(data['max_drawdown'])
+    svg_overwrite('dark_ver.svg', age_data, cagr, total_performance, alpha, beta, sharpe, max_drawdown)
+    svg_overwrite('light_ver.svg', age_data, cagr, total_performance, alpha, beta, sharpe, max_drawdown)
